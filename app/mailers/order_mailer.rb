ac674dd2c3f827bbc899102e303342af00431518
John Geiger <j.geiger@whitesunrise.com>
2018-01-02 13:21:55 -0500

    testing

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,6 @@ class OrderMailer < ApplicationMailer
 
   def order_updated_email(user)
     @user = user
-    mail(to: @user.email, subject: "Your " + ENV['SITE_NAME'] + ' Order Has Been Updated")
+    mail(to: @user.email, subject: "Your #{ENV['SITE_NAME']} Order Has Been Updated")
   end
 end
